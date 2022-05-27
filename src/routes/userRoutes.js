a58a7c47c7a5c89be9552dc154e54d0d5983efbf
kashanmahmood <kashan.mahmood.24@dartmouth.edu>
2022-05-27 14:47:32 -0400

    new commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import * as Users from '../controllers/user_controller';
-import { requireAuth, requireSignin } from '../services/passport';
+// import { requireSignin } from '../services/passport';
 
 const router = Router();
 
@@ -8,14 +8,14 @@ router.get('/', (req, res) => {
   res.json({ message: 'welcome to our user router!' });
 });
 
-router.post('/signin', requireSignin, async (req, res) => {
-  try {
-    const token = Users.signin(req.user);
-    res.json({ token, email: req.user.email });
-  } catch (error) {
-    res.status(422).send({ error: error.toString() });
-  }
-});
+// router.post('/signin', async (req, res) => {
+//   try {
+//     const token = Users.signin(req.user);
+//     res.json({ token, email: req.user.email });
+//   } catch (error) {
+//     res.status(422).send({ error: error.toString() });
+//   }
+// });
 
 // Create User
 router.post('/signup', async (req, res) => {
@@ -28,7 +28,7 @@ router.post('/signup', async (req, res) => {
 });
 
 // Get all users
-router.get('/', async (req, res) => {
+router.get('/all', async (req, res) => {
   try {
     const result = await Users.getUsers();
 
@@ -41,7 +41,7 @@ router.get('/', async (req, res) => {
 // **************** SO WHAT DO WE WANNA DO FOR UPDATE. HOW TO GO ABOUT IT? *******************
 // HOW DO I TAKE CARE OF MULTIPLE FIELDS. huh?
 // HOW WILL AUTHENTICATIONWORK WITH THISSSS
-router.put('/:id', requireAuth, async (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const result = await Users.updateUser(req.params.id, req.body);
 
